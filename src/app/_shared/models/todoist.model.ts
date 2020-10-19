8ec888b48cd8d744e2496db17c0fa2e4e82836d2
Duman Daulbekov <dumandaulbekov@gmail.com>
2020-10-19 17:00:07 +0500

    update todo service and model

--- src/app/_shared/models/todoist.model.ts
+++ src/app/_shared/models/todoist.model.ts
@@ -5,5 +5,29 @@ export interface ITodoist {
     modifiedDate: Date;
     scheduleDate: Date;
     isFinished: boolean;
-    boardType: 'todo' | 'test' | 'done';
+    boardType: 'todo' | 'progress' | 'done';
+}
+
+export interface ITodoChangeName {
+    id: number;
+    modifiedDate: Date;
+    name: string;
+}
+
+export interface ITodoFinished {
+    id: number;
+    modifiedDate: Date;
+    isFinished: boolean;
+}
+
+export interface ITodoChangeBoardType {
+    id: number;
+    modifiedDate: Date;
+    boardType: 'todo' | 'progress' | 'done';
+}
+
+export interface ITodoChangeScheduleDate {
+    id: number;
+    modifiedDate: Date;
+    scheduleDate: Date;
 }
--- src/app/_shared/services/todoist.service.ts
+++ src/app/_shared/services/todoist.service.ts
@@ -2,17 +2,38 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { ITodoist } from '../models/todoist.model';
+import { ITodoChangeName, ITodoFinished, ITodoist } from '../models/todoist.model';
 
 @Injectable({ providedIn: 'root' })
 
 export class TodoistService {
+
     constructor(
         private http: HttpClient
     ) { }
 
+    public getAll(): Observable<ITodoist[]> {
+        return this.http.get<ITodoist[]>(`${environment.PHP_TODOIST_API}/getAll.php`);
+    }
+
     public create(body: ITodoist): Observable<ITodoist> {
         return this.http.post<ITodoist>(`${environment.PHP_TODOIST_API}/create.php`, body);
+    }
+
+    public finished(body: ITodoFinished[]): Observable<ITodoFinished[]> {
+        return this.http.put<ITodoFinished[]>(`${environment.PHP_TODOIST_API}/finished.php`, body);
+    }
+
+    public editName(body: ITodoChangeName): Observable<ITodoChangeName> {
+        return this.http.put<ITodoChangeName>(`${environment.PHP_TODOIST_API}/updateTodo.php`, body);
+    }
+
+    public getById(): Observable<ITodoist> {
+        return this.http.get<ITodoist>(`${environment.PHP_TODOIST_API}/getById.php`);
+    }
+
+    public delete(id: number): Observable<void> {
+        return this.http.delete<void>(`${environment.PHP_TODOIST_API}/delete.php/?id=${id}`);
     }
 
 }
