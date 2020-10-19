a90748606e79a8b5d317aa5f670c6a151aee4646
Duman Daulbekov <dumandaulbekov@gmail.com>
2020-10-19 16:59:48 +0500

    update todoist component

--- src/app/todoist/todoist.component.ts
+++ src/app/todoist/todoist.component.ts
@@ -1,9 +1,11 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { ITodoist } from '../_shared/models/todoist.model';
+import { ITodoChangeName, ITodoFinished, ITodoist } from '../_shared/models/todoist.model';
 import { TodoistService } from '../_shared/services/todoist.service';
 import { CreateTodoDialogComponent } from './create-todo-dialog/create-todo-dialog.component';
+import { EditTodoDialogComponent } from './edit-todo-dialog/edit-todo-dialog.component';
 
 @Component({
   selector: 'app-todoist',
@@ -11,34 +13,48 @@ import { CreateTodoDialogComponent } from './create-todo-dialog/create-todo-dial
   styleUrls: ['./todoist.component.scss']
 })
 export class TodoistComponent implements OnInit {
-  todo = [
-    'Add text for about',
-    'Create first post',
-    'Reread docs about php'
-  ];
-
-  test = [
-    'Fall asleep',
-  ];
-
-  done = [
-    'Check e-mail',
-    'Create template for todoist',
-  ];
+  public todo: ITodoist[] = [];
+  public progress: ITodoist[] = [];
+  public done: ITodoist[] = [];
 
   constructor(
-    private todoistService: TodoistService,
-    public dialog: MatDialog
+    private readonly todoistService: TodoistService,
+    private readonly dialog: MatDialog
 
   ) { }
 
   ngOnInit(): void {
+    this.todoistService.getAll().subscribe({
+      next: (todoist) => {
+        todoist.forEach((todo) => {
+          if (todo.boardType === 'todo') {
+            this.todo.push(todo);
+          }
+
+          if (todo.boardType === 'progress') {
+            this.progress.push(todo);
+          }
+
+          if (todo.boardType === 'done') {
+            this.done.push(todo);
+          }
+        });
+      },
+
+      error: (error: HttpErrorResponse) => console.log('todo get all error', error)
+    });
   }
 
   public drop(event: CdkDragDrop<string[]>): void {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
+      console.log(event.previousContainer.data);
+
+      event.container.data.forEach((todo) => {
+        const x = todo[`boardType`];
+      });
+
       transferArrayItem(event.previousContainer.data,
         event.container.data,
         event.previousIndex,
@@ -47,13 +63,85 @@ export class TodoistComponent implements OnInit {
   }
 
   public createTodoDialog(): void {
-    const dialogRef = this.dialog.open(CreateTodoDialogComponent,{ 
+    const dialogRef = this.dialog.open(CreateTodoDialogComponent, {
       width: '560px'
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
+      if (result) {
+        this.todo.push(result);
+      }
     });
   }
 
+  public editTodoDialog(todo: ITodoChangeName, boardType: 'todo' | 'progress' | 'done'): void {
+    const dialogRef = this.dialog.open(EditTodoDialogComponent, {
+      width: '560px',
+      data: todo
+    });
+
+    dialogRef.afterClosed().subscribe(response => {
+      if (response) {
+        switch (boardType) {
+          case 'todo':
+            this.todo.map((x) => {
+              if (x.id === response.id) {
+                x.name = response.name;
+                x.modifiedDate = response.modifiedDate;
+              }
+            });
+            break;
+          case 'progress':
+            this.progress.map((x) => {
+              if (x.id === response.id) {
+                x.name = response.name;
+                x.modifiedDate = response.modifiedDate;
+              }
+            });
+            break;
+          case 'done':
+            this.done.map((x) => {
+              if (x.id === response.id) {
+                x.name = response.name;
+                x.modifiedDate = response.modifiedDate;
+              }
+            });
+            break;
+        }
+      }
+    });
+  }
+
+  public deleteTodo(todoId: number, boardType: 'todo' | 'progress' | 'done'): void {
+    this.todoistService.delete(todoId).subscribe(() => {
+      switch (boardType) {
+        case 'todo':
+          this.todo = this.todo.filter((todo) => todo.id !== todoId);
+          break;
+        case 'progress':
+          this.progress = this.progress.filter((todo) => todo.id !== todoId);
+          break;
+        case 'done':
+          this.done = this.done.filter((todo) => todo.id !== todoId);
+          break;
+      }
+    });
+  }
+
+  public finishedTodo(selected: ITodoFinished[]): void {
+    const todoist: ITodoFinished[] = [];
+
+    selected.forEach((todo) => {
+      const isFinishedValue: ITodoFinished = todo[`value`];
+      todoist.push({
+        id: isFinishedValue.id,
+        isFinished: true,
+        modifiedDate: new Date()
+      });
+    });
+
+    this.todoistService.finished(todoist).subscribe((x) => {
+      console.log('x', x);
+    });
+  }
 }
