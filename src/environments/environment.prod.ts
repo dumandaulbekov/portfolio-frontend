cbd31e37f3411cf5e63d4715ab6b943584657253
Duman Daulbekov <dumandaulbekov@gmail.com>
2020-10-28 15:35:00 +0500

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   PHP_POST_API: 'https://untitledproject1.kz/portfolio/portfolio-backend/api/post',
-  PHP_TODOIST_API: 'https://untitledproject1.kz/portfolio/portfolio-backend/api/todoist'
+  PHP_TODO_API: 'https://untitledproject1.kz/portfolio/portfolio-backend/api/todo'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   PHP_POST_API: 'http://localhost/portfolio/portfolio-backend/api/post',
-  PHP_TODOIST_API: 'http://localhost/portfolio/portfolio-backend/api/todoist'
+  PHP_TODO_API: 'http://localhost/portfolio/portfolio-backend/api/todo'
 };
