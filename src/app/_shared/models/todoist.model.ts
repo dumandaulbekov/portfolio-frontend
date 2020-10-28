8cac470cbd7581c7a5b5ea8ef27ab2f7102f83e7
Duman Daulbekov <dumandaulbekov@gmail.com>
2020-10-28 15:36:55 +0500

    Update todo model

--- src/app/_shared/models/todoist.model.ts
+++ src/app/_shared/models/todoist.model.ts
@@ -1,33 +1,29 @@
 export interface ITodoist {
     id?: number;
     name: string;
-    createdDate: Date;
-    modifiedDate: Date;
+    createdDate?: Date;
+    modifiedDate?: Date;
     scheduleDate: Date;
-    isFinished: boolean;
+    isFinished?: boolean;
     boardType: 'todo' | 'progress' | 'done';
 }
 
 export interface ITodoChangeName {
     id: number;
-    modifiedDate: Date;
     name: string;
 }
 
 export interface ITodoFinished {
     id: number;
-    modifiedDate: Date;
     isFinished: boolean;
 }
 
 export interface ITodoChangeBoardType {
     id: number;
-    modifiedDate: Date;
     boardType: 'todo' | 'progress' | 'done';
 }
 
 export interface ITodoChangeScheduleDate {
     id: number;
-    modifiedDate: Date;
     scheduleDate: Date;
 }
